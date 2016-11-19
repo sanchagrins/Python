31f5e3301b87cf447e580f30775072006650773a
S. Sanchagrin <sanchagrins@gmail.com>
2016-11-19 19:48:18 +0900

    Comments and code cleanup

--- wikiDino.py
+++ wikiDino.py
@@ -1,61 +1,67 @@
 #!/usr/bin/env python
-# File:        linkGrabber.py
+# File:        wikiDino.py
 # Author:      Stephen
-# Date:        11/11/2016
-# Purpose:     Grabs all links from a given URL.
+# Date:        11/1/2016
+# Purpose:     Scrapes wikipedia for a list of dinosaurs and
+#              converts to xml.
 # Libraries:   BeautifulSoup4
 
+# Library Imports
 import urllib
 import xml.etree.cElementTree as ET
 import xml.dom.minidom
 from bs4 import BeautifulSoup
 
-#url = raw_input('Enter URL: ')
+# Sets wikipedia URL
 url = 'https://en.wikipedia.org/wiki/List_of_dinosaur_genera'
 
+# Preps Beautiful Soup for scraping
 html = urllib.urlopen(url).read()
 soup = BeautifulSoup(html, "html.parser")
-
 tags = soup('a')
 
+# Sets local variables
 tot = 0
 val = 0
 count = 1
 titles = []
 excludes = ["bird", "reptile", "fish", "NoTitle", "Enlarge", 'Help:Category', 'Wikipedia:About']
 dinoList = []
+
+# Start scraping tags
 for tag in tags:
     title = tag.get('title', 'NoTitle')
-    #title.encode('ascii', 'ignore')
     titles.append(title)
+
+# Refines scraped elements
 for t in titles:
+    # Checks for excluded items
     if t not in excludes:
-        #print t
         words = t.split()
+        # Removes enteries longer than one word
         if len(words) == 1:
             dinoList.append(words)
+            # Removes duplicates
             flat = [item for sublist in dinoList for item in sublist]
             flat = set(flat)
-print flat
+print "Scraped " + len(flat) + "elements from " + url
 
+# XML output prep
 root = ET.Element("dinosaurs")
 child = ET.SubElement(root, "dinosaur")
 name = ET.SubElement(child, "Name")
 ID = ET.SubElement(child, "ID")
 
+# Reads through list, converts to XML
 for d in flat:
     count = count + 1
     did = str(count)
     child = ET.Element("dinosaur", ID=did)
     root.append(child)
     element = ET.SubElement(child, "Name").text= d
-    #ET.SubElement(child, "ID").text = did
-    #ET.SubElement(root, "NAME", ID=did).text = d
-    #ET.SubElement(child, "NAME").text = d
-    #ET.SubElement(name, "NAME").text = d
-    #ET.SubElement(ID, "ID")
 
+# Cleans up and writes XML file
 xmlstr = xml.dom.minidom.parseString(ET.tostring(root)).toprettyxml()
-with open("NewXML.xml", "w") as f:
+with open("dinoXML.xml", "w") as f:
     f.write(xmlstr)
 
